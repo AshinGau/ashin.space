2e33c49465e344d318a995efaea12e24042bf5b1
helloxiyue <helloxiyue@foxmail.com>
2016-05-13 12:00:30 +0800

    init website

---
+++ gruntfile.js
@@ -0,0 +1,31 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        less: {
+            development: {
+                options: {
+                    cleancss: true
+                },
+                files: {
+                    "public/css/nano-bootstrap.css1.1.0": "public/less/bootstrap.less",
+                    "public/css/nano-bootstrap.patch.css1.1.0": "public/less/patch.less",
+                    "public/css/ashin.css": "public/less/ashin.less"
+                }
+            }          
+        },
+        watch: {
+            files: ['public/less/*.less','public/less/mixins/*.less'],
+            tasks: ['less'],
+            options: {
+                spawn: false,
+            }
+        }
+    });
+    
+    // 加载包含 "uglify" 任务的插件。
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    // 默认被执行的任务列表。
+    grunt.registerTask('default', ['less','watch']);
+
+}
---
+++ routes/root.js
@@ -0,0 +1,49 @@
+﻿var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+
+router.get('/', function (req, res) {
+    res.render('ashin/home', {
+        'webTitle':'阿xin的空间'
+    });
+});
+
+router.get('/manager/write_log', function (req, res) {
+    res.render('ashin/write_log', {
+        'webTitle':'阿xin的空间·发布日志'
+    });
+});
+
+router.get('/pithyUpload', function (req, res) {
+    res.render('pithyUpload', {
+        'webTitle':'pithyUpload'
+    });
+});
+
+router.post('/pithyUpload', function (req, res) {
+    for(key in req.body)
+        console.log(key);
+    console.log(req.body, req.query);
+    res.send("ok");
+});
+
+/* GET users listing. */
+
+/*
+router.get('/btn', function (req, res) {
+    res.render('btn', {
+        webTitle: '按钮'
+    });
+});
+
+router.get('/input', function (req, res) {
+    var input_html = fs.readFileSync('./source/code/input_html.html', 'utf-8');
+
+    res.render('input', {
+        webTitle: '输入框',
+        htmlCode:input_html
+    });
+});
+*/
+
+module.exports = router;
